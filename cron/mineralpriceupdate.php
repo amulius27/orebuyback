b37310a27d6f88637fa3ff822a9f3020357aedad
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-11 22:43:43 -0500

    minerals

--- cron/mineralpriceupdate.php
+++ cron/mineralpriceupdate.php
@@ -10,21 +10,21 @@ require_once __DIR__.'/../functions/registry.php';
 printf("Performed require_once.<br>");
 
 $itemIDs = array(
-    "tritanium" => "34",
-    "pyerite" => "35",
-    "mexallon" => "36",
-    "isogen" => "37",
-    "nocxium" => "38",
-    "zydrine" => "39",
-    "megacyte" => "40",
-    "morphite" => "11399",
-    "heliumIsotopes" => "16274",
-    "nitrogenIsotopes" => "17888",
-    "oxygenIsotopes" => "17887",
-    "hydrogenIsotopes" => "17889",
-    "liquidOzone" => "16273",
-    "heavyWater" => "16272",
-    "strontiumClathrates" => "16275"    
+    "tritanium" => 34,
+    "pyerite" => 35,
+    "mexallon" => 36,
+    "isogen" => 37,
+    "nocxium" => 38,
+    "zydrine" => 39,
+    "megacyte" => 40,
+    "morphite" => 11399,
+    "heliumIsotopes" => 16274,
+    "nitrogenIsotopes" => 17888,
+    "oxygenIsotopes" => 17887,
+    "hydrogenIsotopes" => 17889,
+    "liquidOzone" => 16273,
+    "heavyWater" => 16272,
+    "strontiumClathrates" => 16275,  
 );
 printf("Set itemIDs up.<br>");
 
@@ -36,7 +36,7 @@ printf("Opened the database.<br>");
 $i = 1;
 
 foreach($itemIDs as $id) {
-    $url = "http://api.eve-central.com/api/marketstat?typeid=".$id."&regionlimit=".$regionlimit;
+    $url = "http://api.eve-central.com/api/marketstat?typeid=" . $id . "&regionlimit=" . $regionlimit;
     $xml = simplexml_load_file($url);
     $price = $xml->marketstat->type->buy->median[0];
     printf("Got $price from Eve-Central.com.<br>");
