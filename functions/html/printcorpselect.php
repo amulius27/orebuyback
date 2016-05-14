660997e32106f53e8493ede0720014c435e8c851
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-14 01:35:37 -0500

    Corrected printcorpselect.php

--- functions/html/printcorpselect.php
+++ functions/html/printcorpselect.php
@@ -5,8 +5,8 @@ function PrintCorpSelect() {
     $corps = $db->fetchColumnMany('SELECT CorpName FROM Corps WHERE Deleted= :quit', array('quit' => 0));
     DBClose($db);
     printf("<div class=\"container\">");
-    printf("<select class=\"form-control\" onchange=\"setCorp(this.value)\">");
-    printf("<option> </option");
+    printf("<select class=\"form-control col-md-5\" onchange=\"setCorp(this.value)\">");
+    printf("<option>Not in Warped Intentions</option>");
     foreach($corps as $corp) {
         printf("<option>$corp</option>");
     }
