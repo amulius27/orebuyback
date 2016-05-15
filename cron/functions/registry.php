32115b0b09387b9f46eee4e7668824a3a489c519
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-14 21:20:03 -0500

    added registry for cron jobs

---
+++ cron/functions/registry.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Autoload functionality
+require_once __DIR__.'/../../vendor/autoload.php';
+
+//Database functionality
+require_once __DIR__.'/../functions/database/dbclose.php';
+require_once __DIR__.'/../functions/database/dbopen.php';
+
+?>
