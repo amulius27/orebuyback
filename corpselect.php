56632ad93d11727ba42fb670277312368a9393d8
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-15 00:22:21 -0500

    sessions

--- corpselect.php
+++ corpselect.php
@@ -6,7 +6,7 @@ session_start();
 $corpTemp = $_REQUEST["corp"];
 $_SESSION["corporation"] = $corpTemp;
 if(isset($_SESSION["corporation"])) {
-    echo "Corp Set";
+    echo "Corp Set to " . $_SESSION["corporation"];
 } else {
     echo "Corp Not Set";
 }
