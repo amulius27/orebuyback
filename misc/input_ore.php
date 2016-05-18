4f97f7800f61f52fec2eff095d8c9736c7d609b4
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-17 21:39:53 -0500

    input_ore.php

--- misc/input_ore.php
+++ misc/input_ore.php
@@ -80,7 +80,7 @@
     $Mercoxit = $value * $Mercoxit;
 
 ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+    
 
     <script>
 	var veldspar = <?= $Veldspar ?>;
