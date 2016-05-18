8cdf1fe359c85dc9363582806e9c578655ab4b3b
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-18 17:27:01 -0500

    t2 pi removed custom.js

--- pi_t2.php
+++ pi_t2.php
@@ -469,7 +469,6 @@
     <!-- Clipboard -->
 
     <script src="js/jquery.cookie.js"></script>
-    <script src="js/custom.js"></script>
     <script src="js/typeahead.bundle.js"></script>
     <script src="js/handlebars-v1.3.0.js"></script>
     <script src="js/t2_pi_cal.js"></script>
