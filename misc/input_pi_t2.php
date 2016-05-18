497b66e2e148c036acc911033f811abe904b7e49
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-18 17:03:22 -0500

    t2 pi

--- misc/input_pi_t2.php
+++ misc/input_pi_t2.php
@@ -60,6 +60,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
 <script>
+    
     var biocells = <?= $biocells ?>;
     var constructionBlocks = <?= $construction_blocks ?>;
     var consumerElectronics = <?= $consumer_electronics ?>;
