635b68389f95f8a7e79be4b654a98e1e953f2e39
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-18 00:24:31 -0500

    t1 pi

--- pi_t1.php
+++ pi_t1.php
@@ -8,10 +8,10 @@
 <html lang="en">
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-    <meta content="Lone Star Buyback Calculator" name="description">
+    <meta content="Warped Intentions Buy Back Program" name="description">
     <meta content="index,follow" name="robots">
     <meta content="width=device-width, initial-scale=1" name="viewport">
-    <title>Buy Up Indexes | Lone Star Warriors</title>
+    <title>Warped Intentions Buy Back Program</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link href="css/custom.css" rel="stylesheet">
@@ -26,7 +26,7 @@
             background-attachment: fixed;
         }
         .affix {
-            top: 60px;
+            top: 75px;
         }
         .affix-bottom {
             position: absolute;
@@ -73,28 +73,52 @@
 </div>
 <!--Navigation-->
 
+<div class="central-header">
+    <h1>Buying to make your lives easier.</h1>
+    <h4 class="text-danger">These pages are still undergoing heavy development. Report any issues on our public
+        forums!</h4>
+</div>
 <div class="container">
     <div class="panel panel-default">
         <div class="panel-heading" align="center">
             <h3 class="panel-title"><span style="font-family: Arial; color: #FF2A2A;"><strong>Instruction Sheet</strong></span><br></h3>
         </div>
         <div class="panel-body" align="center">
-            - In the Calculator below you can enter the amounts for each Ore that you want to sell.<br>
-            - Once done click on the <strong>Invoice</strong> price to submit the contract.<br>
-            - The contract will be submitted to the database, and contract details will be printed on the next page.<br>
-            <span style="font-family: Arial; color: #FF2A2A;"><strong>- Contract max is 500m ISK at a time, will allow for faster processing of the contracts.</strong></span>
-            <hr>
-            <span style="font-family: Arial; color: #8FEF2F;"><strong>Database was last updated on: <?php echo $update ?></strong></span><br>
-            <span style="font-family: Arial; color: #8FEF2F;"><strong>Ore prices are mineral based</strong></span><br>
-            <span style="font-family: Arial; color: white;"><strong>Corporation: </strong> <?php echo $corporation ?></span><br>
-            <span style="font-family: Arial; color: white;"><strong>Alliance Tax Rate: </strong>  <?php echo $alliance_tax ?> %</span><br>
-            <span style="font-family: Arial; color: white;"><strong>Corp Tax Rate: </strong>  <?php echo $corpTax ?> %</span><br>
-            <span style="font-family: Arial; color: white;"><strong>TotalTax Rate: </strong>  <?php echo $total_tax ?> %</span><br>
+            - In the Calculator below you can enter the amounts for each Raw PI material that you want to sell.<br>
+            - Once done you can see the total value of each item and the final price in the <strong>Invoice</strong>
+            panel.<br>
+            - All you have to do now is click on the <strong>Contract Value</strong> price that is given<br>
+            - This will open a "Copy to clipboard" window with a easy to copy number to use in the EVE Contract window.<br>
+            <span style="font-family: Arial; color: #FF2A2A;"><strong>- Contract between 200-300 mil ISK at a time, this to allow for faster processing of the contracts.</strong></span>
+        <span style="font-family: Arial; color: #8FEF2F;"><strong>Database was last updated
+                on: <?php echo $string = implode("", $update[0]); ?></strong></span>
         </div>
     </div>
 </div>
 
 <div class="clearfix"></div>
+
+<div class="container">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Contract Type</th>
+            <th class="text-right">Availability</th>
+            <th class="text-right">I will receive</th>
+            <th class="text-right">Expire</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Item Exchange</td>
+            <td class="text-right">My Corporation or <strong><span class="eve-link" onmouseover="popCorp($(this), 98259161, 'Lone Star Warriors');">Lone Star Warriors</span></strong>
+            </td>
+            <td class="text-right">Value from Invoice below</td>
+            <td class="text-right">2 Weeks</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 
 <!-- Calculate -->
 
@@ -233,6 +257,14 @@
             <div class="panel panel-default" data-spy="affix" data-offset-top="450" data-offset-bottom="370" id="invoice-panel">
                 <div class="panel-heading">
                     <h3 class="panel-title"><strong>Invoice</strong>
+                        <label data-html="true" data-original-title="<b>Fees</b>" class="popover-reward text-info"
+                               data-toggle="popover" data-content="
+                    <span>
+			<p>In this invoice window you can see how the price is build up for each individual mineral</p>
+			<p>The <strong>Contract Value</strong> price is the total of the material values.</p>
+			<hr>
+                        <p>The <b>Contract Value</b> is what you have to use as <i>'I will receive'</i> in the contract.</p>
+                    </span>">[?]</label>
                     </h3>
                 </div>
                 <div class="panel-body">
@@ -253,8 +285,9 @@
                     <p id="calc-output-row">Total Water value <span class="pull-right"><span id="calc-output-water-value"></span></p>
                     <hr>
                     <p id="calc-output-reward-row">
-                        <b>Contract Value    </b><strong class="pull-right" id="calc-output-reward-value"></strong><br>
-                        <br><input class="form-contorl pull-left" type="submit" value="Submit Contract">
+                        <b>Contract Value</b> <a href="#" class="pull-right" onclick="$('#clipboard').modal('show');$('#clipboard-content').val(calcNow()).select();">
+                            <strong id="calc-output-reward-value"></strong>
+                        </a>
                     </p>
                     <br>
                 </div>
@@ -265,11 +298,94 @@
 <!-- Calculate -->
 
 <!-- Footer -->
-<?php
-    PrintFooter();
-    PrintPopups();
-?>
+<div class="clearfix"></div>
+
+<div class="container">
+    <div class="panel panel-default">
+        <div class="panel-heading" align="center">
+            <h3 class="panel-title"><strong>Buy Up Indexes brought to you by <span class="eve-link" onmouseover="popCorp($(this), 98259161, 'Lone Star Warriors');">Lone Star Warriors</span></strong></h3>
+        </div>
+        <div class="panel-body" align="center">
+            <ul class="social">
+                <li>
+                    <a href=
+                       "https://www.facebook.com/lonestarwarriorsgaming"><i class="fa">
+                        </i></a>
+                </li>
+
+                <li>
+                    <a href="https://twitter.com/lone_warriors"><i class=
+                                                                   "fb"></i></a>
+                </li>
+
+                <li>
+                    <a href=
+                       "https://plus.google.com/+lonestarwarriorsgaming"><i class="fc">
+                        </i></a>
+                </li>
+            </ul>
+
+            <p>
+                2015 Design by <a href="#">Joery Pigmans</a>. All rights
+                reserved.
+            </p>
+
+            <p>
+                EVE Online and the EVE logo are the registered trademarks
+                of CCP hf. All rights are reserved worldwide. All other
+                trademarks are the property of their respective owners. EVE
+                Online, the EVE logo, EVE and all associated logos and
+                designs are the intellectual property of CCP hf. All
+                artwork, screenshots, characters, vehicles, storylines,
+                world facts or other recognizable features of the
+                intellectual property relating to these trademarks are
+                likewise the intellectual property of CCP hf. CCP hf. has
+                granted permission to Joery Pigmans to use EVE Online and
+                all associated logos and designs for promotional and
+                information purposes on its website but does not endorse,
+                and is not in any way affiliated with, Joery Pigmans. CCP
+                is in no way responsible for the content on or functioning
+                of this website, nor can it be liable for any damage
+                arising from the use of this website.
+            </p>
+        </div>
+    </div>
+    <!-- Footer -->
+
     <!-- Popups -->
+    <div class="hide" id="popcorp" style="background:white;">
+        <table style='border: 1px solid;'>
+            <thead>
+            <tr style='border-bottom: 1px solid;'>
+                <th colspan='3' style='padding-left:2px;' id="popcorp-name">&nbsp;</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td width='64' height='64' style='padding:2px; background-repeat:no-repeat; background-position:center;' id="popcorp-image">&nbsp;</td>
+                <td style='padding-left:5px; padding-right:5px; padding-top:3px; vertical-align:top;' id="popcorp-content-1">&nbsp;</td>
+                <td style='padding-left:5px; padding-right:5px; padding-top:3px; vertical-align:top;' id="popcorp-content-2">&nbsp;</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- Popups -->
+
+    <!-- Clipboard -->
+    <div class="modal" id="clipboard" tabindex="-1" role="dialog" aria-labelledby="clipboardLabel" aria-hidden="true" onkeydown="if (event.keyCode == 13) $('#clipboard').modal('hide');">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title" id="clipboardLabel">Copy to clipboard: CTRL-C, Enter</h4>
+                </div>
+                <div class="modal-body">
+                    <input type="text" class="form-control text-right" id="clipboard-content">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Clipboard -->
 
     <script src="js/jquery.cookie.js"></script>
     <script src="js/custom.js"></script>
