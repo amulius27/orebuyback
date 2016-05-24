cce1ed4aecfe2ff7eb231325e6b63bff98a18eb5
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-24 01:00:21 -0500

    testing again

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,23 +9,28 @@ require_once ('jpgraph/jpgraph_pie3d.php');
 
 // Some data
 $data = array(40,60,21,33);
-
 // Create the Pie Graph. 
 $graph = new PieGraph(350,250);
-
 $theme_class= new VividTheme;
 $graph->SetTheme($theme_class);
-
 // Set A title for the plot
 $graph->title->Set("A Simple 3D Pie Plot");
-
 // Create
 $p1 = new PiePlot3D($data);
 $graph->Add($p1);
-
 $p1->ShowBorder();
 $p1->SetColor('black');
 $p1->ExplodeSlice(1);
+// Get the handler to prevent the library from sending the
+// image to the browser
+$gdImgHandler = $graph->Stroke(_IMG_HANDLER);
+// Stroke image to a file and browser
+// Default is PNG so use ".png" as suffix
+$fileName = "/images/orequantity.png";
+$graph->img->Stream($fileName);
+// Send it back to browser
+$graph->img->Headers();
+$graph->img->Stream();
 
 
 sec_session_start();
@@ -91,7 +96,7 @@ sec_session_start();
                 <h1 class="page-header">Dashboard</h1>
                 <div class="row placeholders">
                     <div class="col-xs-6 col-sm-3 placeholder">
-                        <?php $graph->Stroke(); ?>
+                        <img src="/images/orequantity.png">
                     </div>
                 </div>
             </div>
