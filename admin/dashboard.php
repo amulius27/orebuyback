90bbc35ef2069eea5cce49d28b97c78eec599a01
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-23 22:36:41 -0500

    dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -10,64 +10,65 @@ sec_session_start();
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Warped Intentions Buy Back Dashboard</title>
-    <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <script src="/js/sha512.js" type="text/javascript"></script>
-    <script src="/js/forms.js" type="text/javascript"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="/css/dashboard.css" rel="stylesheet">
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="/js/ie-emulation-modes-warning.js"></script>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->   
-  </head>
-
-  <body>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <!--metas-->
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta content="Warped Intentions Buy Back Program" name="description">
+    <meta content="index,follow" name="robots">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <title>W4RP Buy Back Program</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/eve-link.css" rel="stylesheet">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        body{
+            background-image:url(images/bgs/ore_bg_blur.jpg);
+            background-repeat:no-repeat;
+            background-attachment: fixed;
+        }
+        .affix {
+            top: 60px;
+        }
+        .affix-bottom {
+            position: absolute;
+        }
+    </style>
+</head>
+<body>
     <?php if (login_check($mysqli) == true) : ?>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        Welcome <?php echo htmlentities($_SESSION['username']); ?>!
-      <div class="container-fluid">
+    <div class="navbar navbar-inverse navbar-fixed-top" style="height: 72px;" role="navigation">
         <div class="navbar-header">
-          <a class="navbar-brand" href="/../index.php">Warped Intentions Buy Back Program</a>
+            <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button> 
+            <a class="navbar-brand" href="/../index.php">
+                <img src="http://image.eveonline.com/Alliance/99004116_64.png" style="margin-top: -10px;">
+            </a>
         </div>
-        <div id="navbar" class="navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="dashboard.php">Dashboard</a></li>
-            <li><a href="contracts.php">Contracts</a></li>
-            <li><a href="corppayouts.php">Corp Payouts</a></li>
-            <li><a href="settings.php">Settings</a></li>
-            <li><a href="profile.php">Profile</a></li>
-          </ul>
+        <div class="collapse navbar-collapse pull-left">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="quote.php">Price Quotes</a></li>
+                <li><a href="http://forums.warpedintentions.com">Forum</a></li>
+            </ul>
         </div>
-      </div>
-    </nav>
+    </div>
 
     
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="/js/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="js/jquery.cookie.js"></script> 
+    <script src="js/eve-link.js"></script>
+            
     
     <?php else : ?>
             <div class="container">
