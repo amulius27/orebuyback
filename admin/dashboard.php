5e82be55f28f35a8f5d32547bc574607bf50c57d
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-23 22:04:14 -0500

    trying dashboard again

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -24,8 +24,8 @@ sec_session_start();
 
     <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
-    <script src="js/sha512.js" type="text/javascript"></script>
-    <script src="js/forms.js" type="text/javascript"></script>
+    <script src="/js/sha512.js" type="text/javascript"></script>
+    <script src="/js/forms.js" type="text/javascript"></script>
     
        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
@@ -34,7 +34,7 @@ sec_session_start();
     <link href="/css/dashboard.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -254,6 +254,12 @@ sec_session_start();
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
     
     <?php else : ?>
             <div class="container">
