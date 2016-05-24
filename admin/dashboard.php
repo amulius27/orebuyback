203f80f925e8262084cfff2c41a445f275847c94
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-23 22:59:07 -0500

    dashboard sidebar

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -63,6 +63,15 @@ sec_session_start();
     
     <div class="container-fluid">
       <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Contracts</a></li>
+            <li><a href="#">Corp Payouts</a></li>
+            <li><a href="#">Corp Settings</a></li>
+          </ul>
+        </div>  
+          
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Dashboard</h1>
 
