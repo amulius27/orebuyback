46bfdfc244f2be8625622fc5987cedcf2aa067b8
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-23 22:00:49 -0500

    dashboard css and js files

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -23,11 +23,25 @@ sec_session_start();
     <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <link href="/css/dashboard.css" rel="stylesheet">
     <script src="js/sha512.js" type="text/javascript"></script>
-    <script src="forms.js" type="text/javascript"></script>
+    <script src="js/forms.js" type="text/javascript"></script>
+    
+       <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/dashboard.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
     
   </head>
 
