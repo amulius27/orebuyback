8ce9bf4ff5e39e0239d2b91c629b4926ac1722bd
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-24 22:07:02 -0500

    death and taxes

--- admin/functions/html/printcorporationpayoutlist.php
+++ admin/functions/html/printcorporationpayoutlist.php
@@ -8,18 +8,21 @@ function PrintCorporationPayoutListAdminDashboard() {
     
     foreach($corporations as $corp) {
         $corporationName = $corp["CorpName"];
+        //Get the taxes paid into the corporation's account
         $paidTaxes = $db->fetchColumn('SELECT sum(Amount) as taxes FROM Corps WHERE CorpName= :corpname AND Type= :type', array('corpname' => $corporationName, 'type' => 0));
+        //Get the taxes already paid out to the corporation
         $paidOutTaxes = $db->fetchColum('SELECT sum(Amount) as taxes FROM Corps WHERE CorpName= :corpname AND Type= :type', array('corpname' => $corporationName, 'type' => 1));
+        //Calculate the taxes left over that are not paid out.
         $taxes = $paidTaxes - $paidOutTaxes;
         //If the taxes are greater than zero, then display the table row for each corporation
-        if($taxes > 0.00) {
+       // if($taxes > 0.00) {
             printf("<tr>");
             printf("<td>" . $corporationName . "</td>");
             printf("<td>" . $taxes . "</td>");
             printf("<td><input type=\"number\" class=\"form-control\" name=\"taxes\"><input type=\"hidden\"  class=\"form-control\" name=\"corporation\" value=\"" . $corporationName . "\"></td>");
             printf("<td><input type=\"submit\" value=\"Process Corp Payout\"</td>");
             printf("</tr>"); 
-        }
+        //}
         
     }
     //Close the database connection
