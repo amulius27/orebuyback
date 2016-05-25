b95eeb522946295c46442cdcd8742fc7a04c1a03
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-25 01:22:10 -0500

    login page update

--- admin/register.php
+++ admin/register.php
@@ -58,42 +58,32 @@ include_once 'includes/functions.php';
                             ?>
                         </div>
                         <div class="panel-body">
-                                <ul>
-                                    <li>Usernames may contain only digits, upper and lower case letters and underscores</li>
-                                    <li>Emails must have a valid email format</li>
-                                    <li>Passwords must be at least 6 characters long</li>
-                                    <li>Passwords must contain
-                                        <ul>
-                                            <li>At least one upper case letter (A..Z)</li>
-                                            <li>At least one lower case letter (a..z)</li>
-                                            <li>At least one number (0..9)</li>
-                                        </ul>
-                                    </li>
-                                    <li>Your password and confirmation must match exactly</li>
-                                </ul>
-                                <form method="post" name="registration_form" action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>">
-                                    Username: <input type='text' class="form-control text-left typeahead" name='username' id='username' /><br>
-                                    Email: <input type="text" class="form-control text-left typeahead" name="email" id="email" /><br>
-                                    Password: <input type="password" class="form-control text-left typeahead" name="password" id="password"/><br>
-                                    Confirm password: <input type="password" class="form-control text-left typeahead" name="confirmpwd" id="confirmpwd" /><br>
-                                    <input type="button" class="form-control" value="Register" onclick="return regformhash(this.form, this.form.username, this.form.email, this.form.password, this.form.confirmpwd);" /> 
-                                </form>
-                                <p>Return to the <a href="index.php">login page</a>.</p>
-
-                                <label>Email</label>
-                                <input type="text" class="form-control text-left typeahead" name="email" id="email">
-                                <label>Password</label>
-                                <input type="password" class="form-control text-left typeahead" name="password" id="password">
-                                <input type="button" class="form-control" value="Login" onclick="formhash(this.form, this.form.password);">
-                             
+                            <ul>
+                                <li>Usernames may contain only digits, upper and lower case letters and underscores</li>
+                                <li>Emails must have a valid email format</li>
+                                <li>Passwords must be at least 6 characters long</li>
+                                <li>Passwords must contain
+                                    <ul>
+                                        <li>At least one upper case letter (A..Z)</li>
+                                        <li>At least one lower case letter (a..z)</li>
+                                        <li>At least one number (0..9)</li>
+                                    </ul>
+                                </li>
+                                <li>Your password and confirmation must match exactly</li>
+                            </ul>
+                            <form method="post" name="registration_form" action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>">
+                                Username: <input type='text' class="form-control text-left typeahead" name='username' id='username' /><br>
+                                Email: <input type="text" class="form-control text-left typeahead" name="email" id="email" /><br>
+                                Password: <input type="password" class="form-control text-left typeahead" name="password" id="password"/><br>
+                                Confirm password: <input type="password" class="form-control text-left typeahead" name="confirmpwd" id="confirmpwd" /><br>
+                                <input type="button" class="form-control" value="Register" onclick="return regformhash(this.form, this.form.username, this.form.email, this.form.password, this.form.confirmpwd);" /> 
+                            </form>
+                            <p>Return to the <a href="index.php">login page</a>.</p>                             
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Registration form to be output if the POST variables are not
-        set or if the registration script caused an error. -->
-        <h1>Register with us</h1>
         
         
     </body>
