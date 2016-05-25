4b97c96a6ca1f3d29ed260a3962da1b318f3669b
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-24 19:13:23 -0500

    dashboard stuff

--- admin/functions/html/printcontractlisting.php
+++ admin/functions/html/printcontractlisting.php
@@ -4,7 +4,7 @@ function PrintContractListAdminDashboard() {
     $db = DBOpen();
     //Get the list of contracts from the database
     $contracts = $db->fetchRow('SELECT * FROM Contracts WHERE Paid= :paid', array('paid' => 0));
-    DBClose($db);
+    
     
     if( $db->getRowCount() > 0 ) {
         foreach($contracts as $contract) {
@@ -17,6 +17,8 @@ function PrintContractListAdminDashboard() {
             printf("</tr>");
         }    
     }
+    
+    DBClose($db);
 }
 
 ?>
