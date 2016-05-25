79d35a76bcebad8913207c8bd5bca1b3e138dc1c
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-24 21:17:56 -0500

    corp payouts listing

--- admin/functions/html/printcorporationpayoutlist.php
+++ admin/functions/html/printcorporationpayoutlist.php
@@ -10,12 +10,15 @@ function PrintCorporationPayoutListAdminDashboard() {
         $paidTaxes = $db->fetchColumn('SELECT SUM(Amount) WHERE CorpName= :corpname AND Type= :type', array('corpname' => $corporationName, 'type' => 0));
         $paidOutTaxes = $db->fetchColum('SELECT SUM(Amount) WHERE CorpName= :corpname AND Type= :type', array('corpname' => $corporationName, 'type' => 1));
         $taxes = $paidTaxes - $paidOutTaxes;
-        printf("<tr>");
-        printf("<td>" . $corporation["CorpName"] . "</td>");
-        printf("<td>" . $taxes . "</td>");
-        printf("<td><input type=\"number\" class=\"form-control\" name=\"taxes\"><input type=\"hidden\" name=\"corporation\" value=\"" . $corporationName . "\"</td>");
-        printf("<td><input type=\"submit\" value=\"Process Corp Payout\"</td>");
-        printf("</tr>");
+        if($taxes > 0.00) {
+            printf("<tr>");
+            printf("<td>" . $corporationName . "</td>");
+            printf("<td>" . $taxes . "</td>");
+            printf("<td><input type=\"number\" class=\"form-control\" name=\"taxes\"><input type=\"hidden\" name=\"corporation\" value=\"" . $corporationName . "\"</td>");
+            printf("<td><input type=\"submit\" value=\"Process Corp Payout\"</td>");
+            printf("</tr>"); 
+        }
+        
     }
     
     
