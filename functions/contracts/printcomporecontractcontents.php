50907114644197f491b1ca629121ebea2c7b87c8
drkthunder02 <mancuso.christopher@gmail.com>
2016-05-29 02:35:51 -0500

    contracts

--- functions/contracts/printcomporecontractcontents.php
+++ functions/contracts/printcomporecontractcontents.php
@@ -2,20 +2,20 @@
 
 function PrintCompOreContractContents($contractNum) {
     $db = DBOpen();
-    $columns = $db->fetchColumnMany('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COUMNS WHERE TABLE_NAME= :table', array('table' => 'CompOreContractContents'));
+    $columns = $db->fetchColumnMany('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME= :table', array('table' => 'CompOreContractContents'));
     $contents = $db->fetchRow('SELECT * FROM CompOreContractContents WHERE ContractNum= :number', array('number' => $contractNum));
     
     $columnsNum = sizeof($colums);
     
     printf("<table class=\"table-striped\">");
     for($i = 0; $i < $columnsNum - 1; $i++) {
-        $header = str_replace('_', ' ', $column);
+        $header = str_replace('_', ' ', $columns[$i]);
         printf("<tr>");
         printf("<td>");
         printf($header);
         printf("</td>");
         printf("<td>");
-        printf($contents[$column]);
+        printf($contents[$columns[$i]]);
         printf("</td>");
         printf("</tr>");
     }
