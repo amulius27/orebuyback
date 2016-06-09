e999936ee42e6ec467f7ebb4e573466697f5ef78
drkthunder02 <mancuso.christopher@gmail.com>
2016-06-08 20:57:10 -0500

    admin panel

--- admin/functions/processes/processnewcorp.php
+++ admin/functions/processes/processnewcorp.php
@@ -4,17 +4,18 @@
     
     $location = 'http://' . $_SERVER['HTTP_HOST'];
     $location = $location . dirname($_SERVER['PHP_SELF']) . '/../../dashboard.php';
-    var_dump($location);
-    $corporation = filter_var(INPUT_POST, $_POST["Corporation"], FILTER_SANITIZE_STRING);
-    $tax = filter_var(INPUT_POST, $_POST["Tax"], FILTER_SANITIZE_STRING);
-    $tax = $tax * 1.00;
-    if($tax > 100.00)
-        $tax = 100.0;
-    if($tax < 0.00)
-        $tax = 0.00;
+    
     
     //Add the new corporation to the database
-    if(is_float($tax)) {
+    if(isset($_POST["Corporation"]) AND isset($_POST["tax"])) {
+        $corporation = filter_var(INPUT_POST, $_POST["Corporation"], FILTER_SANITIZE_STRING);
+        $tax = filter_var(INPUT_POST, $_POST["Tax"], FILTER_SANITIZE_STRING);
+        $tax = $tax * 1.00;
+        if($tax > 100.00)
+            $tax = 100.0;
+        if($tax < 0.00)
+            $tax = 0.00;
+        
         $db = DBOpen();
         $db->insert('Corps', array('CorpName' => $corporation, 'TaxRate' => $tax));
         DBClose($db);
