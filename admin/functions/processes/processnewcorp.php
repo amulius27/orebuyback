ed3c6509e71dc71fddbb6ba24848672f264e767f
drkthunder02 <mancuso.christopher@gmail.com>
2016-06-08 19:22:52 -0500

    admin panel

--- admin/functions/processes/processnewcorp.php
+++ admin/functions/processes/processnewcorp.php
@@ -3,16 +3,21 @@
     require_once __DIR__.'/../../functions/registry.php';
     
     $corporation = filter_var(INPUT_POST, $_POST["Corporation"], FILTER_SANITIZE_STRING);
-    $tax = filter_var(INPUT_POST, $_POST["Tax"], FILTER_SANITIZE_NUMBER_FLOAT);
+    $tax = filter_var(INPUT_POST, $_POST["Tax"], FILTER_SANITIZE_STRING);
+    $tax = $tax * 1.00;
+    if($tax > 100.00)
+        $tax = 100.0;
+    if($tax < 0.00)
+        $tax = 0.00;
     
     //Add the new corporation to the database
     if(is_float($tax)) {
         $db = DBOpen();
         $db->insert('Corps', array('CorpName' => $corporation, 'TaxRate' => $tax));
         DBClose($db);
-        header('Location: /../dashboard.php?msg=newcorpsuccess');
+        header('Location: /../admin/dashboard.php?msg=newcorpsuccess');
     } else {
-        header('Location: /../dashboard.php?msg=newcorpfailure');
+        header('Location: /../admin/dashboard.php?msg=newcorpfailure');
     }
 
 ?>
