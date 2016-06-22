95380cd4e205d8e90ec4dab743185ca3db372229
drkthunder02 <mancuso.christopher@gmail.com>
2016-06-21 19:33:31 -0500

    updates

--- index.php
+++ index.php
@@ -3,11 +3,7 @@
     require_once __DIR__.'/corpselect.php';
     //Start the session
     session_start();    
-    if(isset($_SESSION["corporation"])) {
-        echo $_SESSION["corporation"];
-    } else {
-        echo 'Corp Not Set Yet';
-    }
+    
     PrintHeader();
     PrintHeaderCloseTag();
     PrintBodyTag();
@@ -17,6 +13,12 @@
     PrintIndexPage();
     PrintFooter();
     PrintPopups();
+    if(isset($_SESSION["corporation"])) {
+        echo $_SESSION["corporation"];
+    } else {
+        echo 'Corp Not Set Yet';
+    }
+    
     PrintCloseIndexPage();
 ?>
 
