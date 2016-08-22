e5900e31366253a2c0b64f3fda9efd19a23dfed4
drkthunder02 <mancuso.christopher@gmail.com>
2016-08-22 00:56:02 -0500

    pi t4

--- functions/contracts/pit4contractvalue.php
+++ functions/contracts/pit4contractvalue.php
@@ -3,14 +3,14 @@
 function PiT4ContractValue($update, $corporation, $post) {
     //Get all of the values from the contract update time
     $db = DBOpen();
-    $Broadcast = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2867, 'time' => $update));
-    $Response_Drones = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2868, 'time' => $update));
-    $NanoFactory = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2869, 'time' => $update));
-    $Organic_Mortar_Applicators = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2870, 'time' => $update));
-    $Recursive_Computing = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2871, 'time' => $update));
-    $Power_Core = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2872, 'time' => $update));
-    $Sterile_Conduits = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2875, 'time' => $update));
-    $Mainframe = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2876, 'time' => $update));
+    $BroadcastPrice = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2867, 'time' => $update));
+    $Response_DronesPrice = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2868, 'time' => $update));
+    $NanoFactoryPrice = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2869, 'time' => $update));
+    $Organic_Mortar_ApplicatorsPrice = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2870, 'time' => $update));
+    $Recursive_ComputingPrice = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2871, 'time' => $update));
+    $Power_CorePrice = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2872, 'time' => $update));
+    $Sterile_ConduitsPrice = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2875, 'time' => $update));
+    $MainframePrice = $db->fetchColumn('SELECT Price FROM PiPrices WHERE ItemId= :id AND Time= :time', array('id' => 2876, 'time' => $update));
     
 //Get the last contract number
     $lastContractNum = $db->fetchColumn('SELECT MAX(ContractNum) FROM Contracts');
@@ -22,17 +22,18 @@ function PiT4ContractValue($update, $corporation, $post) {
     $contractValue = 0.00;
     //Create the ore value array for summing later
     $PiT4Value = array(
-        'Broadcast_Node' => $post['Broadcast_Node'] * $Broadcast,
-        'Integrity_Response_Drones' => $post['Integrity_Response_Drones'] * $Response_Drones,
-        'NanoFactory' => $post['Nanofactory'] * $NanoFactory,
-        'Organic_Mortar_Applicators' => $post['Organic_Mortar_Applicators'] * $Organic_Mortar_Applicators,
-        'Recursive_Computing_Module' => $post['Recursive_Computing_Module'] * $Recursive_Computing,
-        'Self_Harmonizing_Power_Core' => $post['Self_Harmonizing_Power_Core'] * $Power_Core,
-        'Sterile_Conduits' => $post['Sterile_Conduits'] * $Sterile_Conduits,
-        'Wetware_Mainframe' => $post['Wetware_Mainframe'] * $Mainframe,
+        //Make the array with the number gotten from the page then multiply by the price.
+        'Broadcast_Node' => $post['Broadcast_Node'] * $BroadcastPrice,
+        'Integrity_Response_Drones' => $post['Integrity_Response_Drones'] * $Response_DronesPrice,
+        'NanoFactory' => $post['Nanofactory'] * $NanoFactoryPrice,
+        'Organic_Mortar_Applicators' => $post['Organic_Mortar_Applicators'] * $Organic_Mortar_ApplicatorsPrice,
+        'Recursive_Computing_Module' => $post['Recursive_Computing_Module'] * $Recursive_ComputingPrice,
+        'Self_Harmonizing_Power_Core' => $post['Self_Harmonizing_Power_Core'] * $Power_CorePrice,
+        'Sterile_Conduits' => $post['Sterile_Conduits'] * $Sterile_ConduitsPrice,
+        'Wetware_Mainframe' => $post['Wetware_Mainframe'] * $MainframePrice,
     );
     
-    //Add the contract value up from the ore
+    //Add the contract value up from the PiT4 Parts
     foreach($PiT4Value as $value) {
        $contractValue = $contractValue + $value;
     }
@@ -43,8 +44,9 @@ function PiT4ContractValue($update, $corporation, $post) {
     //Calculate the taxes from the contract value
     $allianceTax = $contractValue * ($allianceTaxRate / 100.0);
     $corpTax = $contractValue * ($corpTaxRate / 100.0);
+    $taxes = $allianceTax + $corpTax;
     //Adjust the contract value
-    $contractValue = ($contractValue - $allianceTax) - $corpTax;
+    $adjustedContractValue = ($contractValue - $taxes);
    
    //Set the ore contents array up to be insert into the OreContractContents database
    $piT4Contents = array(
@@ -67,7 +69,7 @@ function PiT4ContractValue($update, $corporation, $post) {
         'ContractType' => 'PiT4',
         'Corporation' => $corporation,
         'QuoteTime' =>  $update,
-        'Value' => $contractValue,
+        'Value' => $adjustedContractValue,
         'AllianceTax' => $allianceTax,
         'CorpTax' => $corpTax
     );
