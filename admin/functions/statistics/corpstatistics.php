d070fa2fc46cd77fcbfa919725e69e92ed3ecf21
drkthunder02 <mancuso.christopher@gmail.com>
2016-08-26 01:05:32 -0500

    corp statistics

--- admin/functions/statistics/corpstatistics.php
+++ admin/functions/statistics/corpstatistics.php
@@ -8,11 +8,11 @@ function GetCorpStats($corp, \Simplon\Mysql\Mysql $db) {
     $paid = $db->fetchColumn('SELECT COUNT(Paid) FROM Contracts WHERE Corporation= :corp AND Paid= :paid AND Deleted= :del', array('corp' => $corp, 'paid' => 1, 'del' => 0));
     
     $statistics = array(
-        "isk" => $isk,
-        "taxes" => $taxes,
-        "contracts" => $contracts,
-        "deleted" => $deleted,
-        "paid" => $paid
+        "isk" => number_format($isk, 2, '.', ','),
+        "taxes" => number_format($taxes, 2, '.', ','),
+        "contracts" => number_format($contracts, 0, '.', ','),
+        "deleted" => number_format($deleted, 0, '.', ','),
+        "paid" => number_format($paid, 0, '.', ',')
     );
     
     return $statistics;
