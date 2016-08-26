829acfae633daa4e93b9910f30d7a8d4017708f0
drkthunder02 <mancuso.christopher@gmail.com>
2016-08-26 01:18:02 -0500

    corp statistics

--- admin/functions/processes/modifycorp.php
+++ admin/functions/processes/modifycorp.php
@@ -8,11 +8,17 @@ $taxRate = filter_input(INPUT_POST, $_POST["Tax"], FILTER_SANITIZE_NUMBER_FLOAT)
 $db = DBOpen();
 //Get the unique index from the table for the corporation being modified.
 $index = $db->fetchColumn('SELECT index FROM Corps WHERE CorpName= :corp', array('corp' => $corporation));
-$db->update('Corps', array('index' => $index), array('TaxRate' => $taxRate));
+var_dump($index);
+printf("<br>");
+$made = $db->update('Corps', array('index' => $index), array('TaxRate' => $taxRate));
+var_dump($made);
+printf("<br>");
 //Close the database connection
 DBClose($db);
 
 //Return to the Modify Contracts page
 $location = 'http://' . $_SERVER['HTTP_HOST'];
 $location = $location . dirname($_SERVER['PHP_SELF']) . '/../../corpsettings.php';
+var_dump($location);
+printf("<br>");
 header("Location: $location");        
