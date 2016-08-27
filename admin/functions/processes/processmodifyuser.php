cc1fb424c3bbc65317d1a5fbc4c7d04fe374f823
drkthunder02 <mancuso.christopher@gmail.com>
2016-08-26 23:35:41 -0500

    modify user

--- admin/functions/processes/processmodifyuser.php
+++ admin/functions/processes/processmodifyuser.php
@@ -1,13 +1,16 @@
 <?php
     require_once __DIR__.'/../../functions/registry.php';
     
+    //Open the database connection
     $db = DBOpen();
     
     //If the user has changed, then modify the user
     $username = filter_input(INPUT_POST, 'users', FILTER_SANITIZE_SPECIAL_CHARS);
     $role = filter_input(INPUT_POST, 'role', FILTER_SANITIZE_SPECIAL_CHARS);
-    
-    $db->update('member_roles', array('username' => $username), array('role' => $role));
+    $currentRole = $db->fetchColumn('SELECT role FROM member_roles WHERE username= :user', array('user' => $username));
+    if($role != $currentRole) {
+        $db->update('member_roles', array('username' => $username), array('role' => $role));
+    }
     
     DBClose($db);
     //Return back to the dashboard
