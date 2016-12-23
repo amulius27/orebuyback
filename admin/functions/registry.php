e6f5c675e907d51cc9eba948b3c96ccc6afa4ea4
drkthunder02 <mancuso.christopher@gmail.com>
2016-12-22 20:29:01 -0600

    modified registry for admin section

--- admin/functions/registry.php
+++ admin/functions/registry.php
@@ -2,25 +2,33 @@
 
 //Autoloader for includes
 require_once __DIR__.'/../../vendor/autoload.php';
+
 //Session Class
 require_once __DIR__.'/../functions/class/sessions.php';
+
 //Database Functions
 require_once __DIR__.'/../functions/database/dbclose.php';
 require_once __DIR__.'/../functions/database/dbopen.php';
+
 //HTML Functions
+require_once __DIR__.'/../functions/html/printmineralrequestform.php';
 require_once __DIR__.'/../functions/html/printcontractlisting.php';
+require_once __DIR__.'/../functions/html/printcorpcontractlist.php';
 require_once __DIR__.'/../functions/html/printcorporationpayoutlist.php';
 require_once __DIR__.'/../functions/html/printcorpselect.php';
-require_once __DIR__.'/../functions/html/printmodifycontractlisting.php';
 require_once __DIR__.'/../functions/html/printdeletecontractlisting.php';
-require_once __DIR__.'/../functions/html/printuserdropdown.php';
+require_once __DIR__.'/../functions/html/printdeletecorpcontract.php';
 require_once __DIR__.'/../functions/html/printhtmlheader.php';
-require_once __DIR__.'/../functions/html/printcompletemienralrequestform.php';
 require_once __DIR__.'/../functions/html/printmineralrequestform.php';
+require_once __DIR__.'/../functions/html/printmodifycontractlisting.php';
+require_once __DIR__.'/../functions/html/printuserdropdown.php';
+
 //System Functions
 require_once __DIR__.'/../functions/system/printnavbar.php';
+
 //Statistic Functions
 require_once __DIR__.'/../functions/statistics/corpstatistics.php';
+
 //Chart Functions
 require_once __DIR__.'/../functions/charts/donutchart.php';
 ?>
