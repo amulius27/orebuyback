0bef6d414a5a45d2ae1ab07db7b8bea850b799e5
drkthunder02 <mancuso.christopher@gmail.com>
2016-12-26 23:09:50 -0600

    removed chart from corpsettings cause I couldn't get it to work.

--- admin/corpsettings.php
+++ admin/corpsettings.php
@@ -3,7 +3,6 @@
     include_once 'includes/db_connect.php';
     include_once 'includes/functions.php';
     require_once __DIR__.'/functions/registry.php';
-    use Khill\Lavacharts;
 
     $session = new Custom\AdminSession\sessions();
 
@@ -15,37 +14,7 @@
     $taxRate = $db->fetchColumn('SELECT TaxRate FROM Corps WHERE CorpName= :corp', array('corp' => $corporation));
 
     $stats = GetCorpStats($corporation, $db);
-
-    //Get contents for chart below here
-    //Initialize the lavachart
-    $chart = new LavaCharts;
-    //Setup the data table to insert data into
-    $data = $chart->DataTable();
-    //Add the two columns for the donut chart
-    $data->addStringColumn('Contract Type');
-    $data->addNumberColumn('Number');
-    //Get the types of contracts
-    $data->addRow(['Minerals', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'Mineral'))]);
-    $data->addRow(['Ore', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'Ore'))]);
-    $data->addRow(['Comp Ore', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'CompOre'))]);
-    $data->addRow(['Ice', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'Ice'))]);
-    $data->addRow(['Ice Products', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'IceProd'))]);
-    $data->addRow(['Fuel', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'Fuel'))]);
-    $data->addRow(['Pi', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'Pi'))]);
-    $data->addRow(['PiT1', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'PiT1'))]);
-    $data->addRow(['PiT2', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'PiT2'))]);
-    $data->addRow(['PiT3', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'PiT3'))]);
-    $data->addRow(['PiT4', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'PiT4'))]);
-    $data->addRow(['Salvage', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'Salvage'))]);
-    $data->addRow(['WGas', $db->fetchColumn('SELECT COUNT(*) FROM Contracts WHERE Corporation= :corp AND ContractType= :type', array('corp' => $corporation, 'type' => 'WGas'))]);
-    
-    //Set the name and attributes of the chart
-    $chart->BarChart('Buyback Contracts', $data, [
-            'title' => 'Contracts Submitted',
-            'backgroundColor' => '#FFCD00',
-            'height' => 600,
-    ]);
-    
+ 
     PrintHTMLHeader('/../images/bgs/ore_bg_blur.jpg');
     printf("<body>");
     if ((login_check($mysqli) == true) AND $role == ('Director' or 'CEO' or 'SiteAdmin')) {
@@ -59,9 +28,9 @@
                         <div class=\"panel-body\" align=\"center\">
                             <form action=\"functions/processes/modifycorp.php\" method=\"POST\">
                                 <label>Tax Rate:</label>
-                                <input type=\"text\" class=\"form-control\" name=\"Tax\" id=\"Tax\" placeholder=\"<?php echo $taxRate; ?>\" />
+                                <input type=\"text\" class=\"form-control\" name=\"Tax\" id=\"Tax\" placeholder=\"" . $taxRate . "\" />
                                 <br>
-                                <input type=\"hidden\" class=\"form-control\" name=\"CorpName\" id=\"CorpName\" value=\"<?php echo $corporation; ?>\">
+                                <input type=\"hidden\" class=\"form-control\" name=\"CorpName\" id=\"CorpName\" value=\"" . $corporation . "\">
                                 <br>
                                 <input type=\"submit\" class=\"form-control\" value=\"Modify Corp\" />
                             </form>
@@ -85,19 +54,6 @@
                         </div>
                     </div>
                 </div>
-                <div class=\"container\">
-                    <div class=\"panel panel-default\">
-                        <div class=\"panel-heading\" align=\"center\">
-                            <h3 class=\"panel-title\"><span style=\"font-family: Arial; color: #FFF;\"><strong><Contract Statistics</strong></span></h3>
-                        </div>
-                        <br>
-                        <div class=\"panel-body\" align=\"left\">
-                             <div id=\"chart-div\"></div>
-                        </div>
-                    </div>
-                </div>
-
-
 
                 <script src=\"/../js/jquery.cookie.js\"></script> 
                 <script src=\"/../js/eve-link.js\"></script>");
@@ -107,13 +63,7 @@
     }
     
     DBClose($db);
-?>
 
-<?=
-    $chart->render('BarChart', 'Number', 'chart-div');
-?>
-
-<?php
     printf("</body>
         </html>");
 ?>
